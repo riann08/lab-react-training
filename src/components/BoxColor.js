b712c8e898a0d5d9df90cec565e9069e51733f8f
Ann Lampa <riann08@yahoo.com>
2021-01-04 23:11:33 +0100

    iteration 4 done

--- src/components/BoxColor.js
+++ src/components/BoxColor.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-const BoxColor = (props) => {
-    return null;
+
+function BoxColor(props) {
+    function ConvertRGBToHex(r,g,b){
+        const Hex = (r << 16 | g << 8 | b).toString(16).toUpperCase();
+        return "#" + Hex;
+    };
+
+    return (
+        <div style=
+        {{backgroundColor: `rgb(${props.r},${props.g},${props.b})`,
+    }}>
+    rgb({props.r}, {props.g}, {props.b}); <br/>
+    {ConvertRGBToHex(props.r,props.g,props.b)};
+        </div >
+        
+    );
 };
 
 export default BoxColor;
